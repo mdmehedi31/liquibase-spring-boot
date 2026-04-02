8d3648e1b14a879afe8ca46f176b36d38776f995
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-04-02 09:39:25 +0600

    liquibase impl

--- src/main/java/com/liquibase/entity/UserEntity.java
+++ src/main/java/com/liquibase/entity/UserEntity.java
@@ -1,4 +1,44 @@
 package com.liquibase.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table tb_user (
+    id int AUTO_INCREMENT  primary key,
+    user_name varchar(50),
+    email varchar(100)
+)
